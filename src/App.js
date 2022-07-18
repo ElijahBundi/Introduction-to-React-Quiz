29ff889f3804cbc58a688caf72913d6676c19751
Elijah <bundielly@gmail.com>
2022-07-18 10:11:49 +0300

    app completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,30 +6,27 @@ import './App.css';
   Challenge: Display all users to the browser
 **/
 
-const users = [
-  { name: "John Doe", id: 1 },
-  { name: "Jane Doe", id: 2 },
-  { name: "Billy Doe", id: 3 }
-];
+
 
 // comment this out after completion and uncomment code below it to proceed
-function Child() {
-  return <div>This is children content</div>;
-}
-/**
-  Challenge: Uncomment this code to complete quiz
+// function Child() {
+//   return <div>This is children content</div>;
+// }
 
-function Child() {
+  // Challenge: Uncomment this code to complete quiz
+
+function Child({ onAddToParent }) {
+
   return (
     <>
       <div>Child</div>
-      <button>Change Parent Value</button>
+      <button onClick={onAddToParent}>Change Parent Value</button>
     </>
   );
 }
 
 function Parent() {
-  const [value, setValue] = React.useState(
+  const [value, setValue] = useState(
     "I need to be updated from my child"
   );
 
@@ -42,39 +39,58 @@ function Parent() {
       </div>
 
       <div className="wrapper">
-        <Child />
+        <Child onAddToParent={() => setValue('Heeeeey!')}/>
       </div>
     </>
   );
 }
-Uncomment this to tackle quiz
-**/
+// Uncomment this to tackle quiz
+
 
 // Comment out after completion
-function Parent() {
-  return (
-    <div>
-      <h3>Parent Component</h3>
-    </div>
-  );
-}
+// function Parent() {
+//   return (
+//     <div>
+//       <h3>Parent Component</h3>
+//     </div>
+//   );
+// }
 // Comment above code after completion
 
+
 function App() {
-  const [] = React.useState(true);
+  
+  const [toggleChallenge, setToggleChallenge] = useState(false);
+  
+  const users = [
+    { name: "John Doe", id: 1 },
+    { name: "Jane Doe", id: 2 },
+    { name: "Billy Doe", id: 3 }
+  ];
+
+  const userNames = users.map((user) => {
+    return <li key={user.id}>{user.name}</li>
+  })
+  
+  function handleClick() {
+    setToggleChallenge(toggleChallenge ? "" : "Toggle Challenge")
+    return toggleChallenge
+  }
+
+
   return (
     
-    <>
-    
-    <img src={logo} className="App-logo" alt="logo" />
+    <> 
+      <h1 style = {{ textDecoration : "underline", color : "red"}}>JSX is cool!</h1>   
+      <img src={logo} className="App-logo" alt="logo" />
       <h3>User names</h3>
-      <ul></ul>
-      <button>Hide Element Below</button>
+      <ul>{userNames}</ul>
+      <button onClick={handleClick} >Hide Element Below</button>
 
-      <div>Toggle Challenge</div>
+      <div>{toggleChallenge}</div>
       <Parent>
       <Child />
-    </Parent>
+      </Parent>
     </>
   );
 }
